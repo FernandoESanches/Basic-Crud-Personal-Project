193fddabef85e644762b434adbc55490ca55af8d
Fernando <fes.sanches@hotmail.com>
2025-09-20 21:17:07 -0300

    refactor: getting rid of the sql URI temporarily

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class DbConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://sql10798766:2klsHXQJ6J@sql10.freesqldatabase.com:3306/sql10798766"
+    SQLALCHEMY_DATABASE_URI = ""
 
